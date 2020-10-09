19773073195a7cbe8ade9e652838ab8fee9621cd
Leigh Nugent <ldnugent44@gmail.com>
2020-10-09 02:11:28 +0100

    created list of ids, removed axes that had not filled properly from Momocs file list, did PCA, kmeans, colour coded panel with clusters and displayed ids

--- scripts/momocs-prelim.R
+++ scripts/momocs-prelim.R
@@ -19,48 +19,64 @@ three_plan <- views %>%
 ids <- three_plan %>% str_extract("[0-9]{2,}[ab]*")
 
 #import to momocs
-plans <- import_jpg(three_plan)
+plans <- import_jpg(correct_files)
 
 #convert to momocs outline/Out class
 outlines <- Out(plans)
 
-#plots all outlines
+#plots all outlines to check for unsuccessful fills
+panel(outlines, names = as.factor(no_errors$id))
 panel(outlines)
 
-outlines %>% stack()
+errors <- c("10034", "10036", "10037", "10040", "10055", "10125", "10148", "10149",
+            "10199", "10229", "10429", "10427", "10421", "10418", "10459", "10461",
+            "10490", "10519", "10521", "10525", "10564", "10571", "10600", "10610",
+            "10628", "10634", "10896", "11038", "11128", "20001", "20012", "50016",
+            "50031", "50054", "50057", "50129", "60064", "60090", "60048", "60097",
+            "60106", "60140", "60145", "60147", "60153", "60182", "60201", "60208", 
+            "60211", "60212", "61014", "61025", "70052", "71002", "71004", "81008", 
+            "81012", "10406", "10157", "10160", "10006", "50047")
+
+axe_lookup <- three_plan %>% 
+  enframe(name = NULL) %>%
+  mutate(id = ids) %>%
+  rename(file = value)
+
+no_errors <- axe_lookup[!axe_lookup$id %in% errors, ]
+
+correct_files <- no_errors$file
 
 #preliminary normalization steps
 aligned <- coo_aligncalliper(outlines)
-
-homog_coo <- coo_sample(aligned, n=40)
-
-homog_coo$ldk <- Ldk(homog_coo)
 
 #checking harmonic power, how many to use in ef
 ef_first <- efourier(outlines[1], 12, norm=FALSE)
 
 ef_mid <- efourier(aligned[599], 12, norm=FALSE)
 
-plot(cumsum(harm_pow(ef_first)[-1]), type='o',
+plot(cumsum(harm_pow(ef_mid)[-1]), type='o',
      main='Cumulated harmonic power without the first harmonic',
      ylab='Cumulated harmonic power', xlab='Harmonic rank')
 
 #analysis
 
-ef <- efourier(outlines, 4)
-ef_aligned <- efourier(aligned, 4, norm=FALSE)
-ef_homog <- efourier(homog_coo, 4, norm=FALSE)
+ef <- efourier(outlines, 6)
+ef_aligned <- efourier(aligned, 6, norm=FALSE)
+
+hcontrib(ef, harm.r=0:20, col="purple",
+         main="lovely axes")
 
 coo_oscillo(aligned[500], "efourier")
 
-pca <- PCA(ef_homog)
+pca <- PCA(ef)
 plot_PCA(pca)
 
-kmeans <- KMEANS(pca, centers = 2,
+
+kmeans <- KMEANS(pca, centers = 3,
                  algorithm = "Hartigan-Wong")
 
 #plots all outlines with clusters coloured
-panel(homog_coo, fac=as.factor(kmeans$cluster), names=as.factor(kmeans$cluster))
-panel(slice(homog_coo, 599:1198), fac=as.factor(kmeans$cluster), names=as.factor(kmeans$cluster))
+panel(outlines, palette=col_spring, 
+      fac=as.factor(kmeans$cluster), names=as.factor(no_errors$id))
 
 
